a1f888ae1661506849f3c1c4a8031075513b32b2
Mariusz <mariusz.blaszczak@gmail.com>
2014-12-15 19:22:21 +0100

    add method random 2 to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,11 @@ class Photo < ActiveRecord::Base
   attr_accessible :title, :url
   validates :url, presence: true
   validates :url, uniqueness: true
+
+  def self.get_two_random(user_cookie = false)
+  	offset1, offset2 = rand(self.count), rand(self.count)
+  	offset2 = rand(self.count) while offset2 == offset1
+		return [self.offset(offset1).first, self.offset(offset2).first]
+  end
+
 end
--- test/unit/photo_test.rb
+++ test/unit/photo_test.rb
@@ -10,4 +10,12 @@ class PhotoTest < ActiveSupport::TestCase
   test "not create photo without url" do 
   	assert Photo.new(url: "").invalid?
   end
+
+  test "return two different photos" do
+  	photos = Photo.get_two_random
+  	assert_not_equal photos.first, photos.last
+  end
+
+
+
 end
