314ca8d14f1b003c1ae5635bda983919f64bb92c
Mariusz <mariusz.blaszczak@gmail.com>
2014-12-16 13:11:26 +0100

    block rating the same photo by one user

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,3 +1,6 @@
 class Rate < ActiveRecord::Base
   attr_accessible :photo_id, :user_id
+  belongs_to :rate
+  validates :photo_id, :uniqueness => {:scope => :user_id}
+
 end
--- test/unit/rate_test.rb
+++ test/unit/rate_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class RateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can not rate the same user the same photo" do
+  	Rate.create(user_id: "a", photo_id: 1)
+    assert Rate.create(user_id: "a", photo_id: 1).invalid?
+  end
 end
