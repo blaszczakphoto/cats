a98872859853787a946a938de75f02d6c4632918
Mariusz <mariusz.blaszczak@gmail.com>
2014-12-16 13:12:39 +0100

    add model method to get best rated photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,11 +2,21 @@ class Photo < ActiveRecord::Base
   attr_accessible :title, :url
   validates :url, presence: true
   validates :url, uniqueness: true
+  has_many :rates
 
   def self.get_two_random(user_cookie = false)
   	offset1, offset2 = rand(self.count), rand(self.count)
   	offset2 = rand(self.count) while offset2 == offset1
 		return [self.offset(offset1).first, self.offset(offset2).first]
+  end
+
+  def self.top_rated_last_week
+  	self.select("photos.id, count(rates.id) as photo_rated").
+		  	joins(:rates).
+		  	where('rates.created_at >= ?', 1.week.ago).
+		  	group("photos.id").
+		  	order("photo_rated DESC").
+		  	limit(3)
   end
 
 end
--- test/unit/photo_test.rb
+++ test/unit/photo_test.rb
@@ -16,6 +16,23 @@ class PhotoTest < ActiveSupport::TestCase
   	assert_not_equal photos.first, photos.last
   end
 
+  test "top rated last week is ordered by ratings" do
+    top = Photo.top_rated_last_week
+    assert top.first.rates.count > top.last.rates.count
+  end
+  
+  test "top rated last week is only from last week" do
+    top = Photo.top_rated_last_week
+    old_top, old_last = top.first, top.last
+    5.times do
+      user_id = rand(8)
+      rate = Rate.create(photo_id: top.last.id, user_id: user_id)
+      rate.created_at = 2.weeks.ago
+      rate.save
+    end
+    top = Photo.top_rated_last_week
+    assert old_top == top.first
+  end
 
 
 end
